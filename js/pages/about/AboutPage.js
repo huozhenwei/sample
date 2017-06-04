a8bd18cb46f791380764c37032bf310f49b4f551
huozhenwei <718289183@qq.com>
2017-06-04 21:36:56 +0800

    6.6 My模块AboutPage公用代码提取到AboutCommon

--- js/pages/about/AboutPage.js
+++ js/pages/about/AboutPage.js
@@ -7,78 +7,22 @@ import {
     Text,
     View,
     Image,
-    Dimensions,
-    Platform
 } from 'react-native';
 
-import ParallaxScrollView from 'react-native-parallax-scroll-view';
 import ViewUtil from '../../util/ViewUtil';
 import {MORE_MENU} from '../../common/MoreMenu';
 import GlobalStyles from '../../../res/styles/GlobalStyles';
+import AboutCommon,{FLAG_ABOUT} from './AboutCommon';
 export default class AboutPage extends Component{
 
     constructor(props) {
         super(props);
+        //组装者模式,把公用代码提取出来
+        this.aboutCommon = new AboutCommon(props,(dic)=>this.updateState(dic),FLAG_ABOUT.flag_about);
     }
-    getParallaxRenderConfig(params){
-        let config = {};
-        config.renderBackground = () => (
-            <View key="background">
-                <Image source={{uri: params.backgroundImg,
-                                width: window.width,
-                                height: PARALLAX_HEADER_HEIGHT}}/>
-                <View style={{position: 'absolute',
-                              top: 0,
-                              width: window.width,
-                              backgroundColor: 'rgba(0,0,0,.4)',
-                              height: PARALLAX_HEADER_HEIGHT}}/>
-            </View>
-        );
-        config.renderForeground=() => (
-            <View key="parallax-header" style={ styles.parallaxHeader }>
-                <Image style={ styles.avatar } source={{
-                  uri: params.avatar,
-                  width: AVATAR_SIZE,
-                  height: AVATAR_SIZE
-                }}/>
-                <Text style={ styles.sectionSpeakerText }>
-                    {params.name}
-                </Text>
-                <Text style={ styles.sectionTitleText }>
-                    {params.description}
-                </Text>
-            </View>
-        );
-        config.renderStickyHeader=() => (
-            <View key="sticky-header" style={styles.stickySection}>
-                <Text style={styles.stickySectionText}>{params.name}</Text>
-            </View>
-        );
-
-        config.renderFixedHeader=() => (
-            <View key="fixed-header" style={styles.fixedSection}>
-                {ViewUtil.getLeftButton(()=>this.props.navigator.pop())}
-            </View>
-        );
-        return config;
+    updateState(dic){
+        this.setState(dic)
     }
-
-    renderView(contentView,params) {
-        let renderConfig = this.getParallaxRenderConfig(params);
-        return (
-          <ParallaxScrollView
-              headerBackgroundColor="#333"
-              backgroundColor = '#2196F3'
-              stickyHeaderHeight={ STICKY_HEADER_HEIGHT }
-              parallaxHeaderHeight={ PARALLAX_HEADER_HEIGHT }
-              backgroundSpeed={10}
-              {...renderConfig}
-          >
-              {contentView}
-          </ParallaxScrollView>
-        )
-    }
-
     onClick(tab){
         let TargetComponent, params = {...this.props,menuType:tab};
         switch (tab){
@@ -87,7 +31,6 @@ export default class AboutPage extends Component{
                 // params.flag = FLAG_LANGUAGE.flag_language;
                 break;
             case MORE_MENU.Website:
-
                 break;
             case MORE_MENU.Feedback:
 
@@ -114,7 +57,7 @@ export default class AboutPage extends Component{
             {this.getItem(MORE_MENU.Feedback, require('../../../res/images/ic_feedback.png'), MORE_MENU.Feedback)}
             <View style={GlobalStyles.line}/>
         </View>;
-        return this.renderView(contentView,{
+        return this.aboutCommon.renderView(contentView,{
             'name':'GitHub Popular',
             'description':'这是一个用来查看GitHub最受欢迎与最热项目的APP,它基于React Native支持Android和iOS双平台.',
             'avatar':'http://avatar.csdn.net/1/1/E/1_fengyuzhengfan.jpg',
@@ -122,70 +65,3 @@ export default class AboutPage extends Component{
         });
     }
 }
-
-const window = Dimensions.get('window');
-
-const AVATAR_SIZE = 120;
-const PARALLAX_HEADER_HEIGHT = 350;
-const STICKY_HEADER_HEIGHT = 70;
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'black'
-    },
-    background: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: window.width,
-        height: PARALLAX_HEADER_HEIGHT
-    },
-    stickySection: {
-        height: STICKY_HEADER_HEIGHT,
-        justifyContent: 'center',
-        alignItems:'center',
-        paddingTop:(Platform.OS === 'ios')?20:0
-    },
-    stickySectionText: {
-        color: 'white',
-        fontSize: 20,
-        margin: 10
-    },
-    fixedSection: {
-        position: 'absolute',
-        bottom: 0,
-        right: 0,
-        left:0,
-        top:0,
-        paddingRight:8,
-        flexDirection:'row',
-        alignItems:'center',
-        paddingTop:(Platform.OS === 'ios')?20:0,
-        justifyContent:'space-between'
-    },
-    fixedSectionText: {
-        color: '#999',
-        fontSize: 20
-    },
-    parallaxHeader: {
-        alignItems: 'center',
-        flex: 1,
-        flexDirection: 'column',
-        paddingTop: 100
-    },
-    avatar: {
-        marginBottom: 10,
-        borderRadius: AVATAR_SIZE / 2
-    },
-    sectionSpeakerText: {
-        color: 'white',
-        fontSize: 24,
-        paddingVertical: 5
-    },
-    sectionTitleText: {
-        color: 'white',
-        fontSize: 16,
-        paddingVertical: 5
-    }
-});
